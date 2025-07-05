c90f751559afea67985ffdf3c67ecf1abde417c0
Jayesh Hadiyal <jayeshhadiyal2007@gmail.com>
2025-07-05 21:44:24 +0530

    append the code of levelOrder traversal

--- Tree/TreeIemplementation.cpp
+++ Tree/TreeIemplementation.cpp
@@ -59,6 +59,38 @@ void postOrder(Node* root) {
     cout << root->data << " ";
 }
 
+
+// LevelOrder traversal
+void levelOrder(Node* root) {
+    queue<Node*> q;
+    q.push(root);
+    q.push(NULL);
+    while(q.size() > 0) {
+        Node* curr = q.front();
+        q.pop();
+
+        if(curr == NULL) {
+            if(!q.empty()) {
+                cout << endl;
+                q.push(NULL);
+                continue;
+            }
+            else {
+                break;
+            }
+        }
+
+        cout << curr->data << " ";
+        if(curr->left != NULL) {
+            q.push(curr->left);
+        }
+        if(curr->right != NULL) {
+            q.push(curr->right);
+        }
+    }
+    cout << endl;
+}
+
 int main (){
     vector<int> preOrder = {1, 2, -1, -1, 3, 4, -1, -1, 5, -1, -1};
     Node* root = buildTree(preOrder);
@@ -67,5 +99,9 @@ int main (){
     inorder(root); // 2 1 4 3 5
     cout << endl;
     postOrder(root); // 2 4 5 3 1
+    cout << endl;
+    levelOrder(root); // 1 
+                      // 2 3 
+                      // 4 5
     return 0;
 }
